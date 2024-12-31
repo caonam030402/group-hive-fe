e96003a34985f18b8064912fa4e2ce00f023e59e
Cao Nam <caonam030402@gmail.com>
2024-12-31 14:37:54 +0700

    feat(get-started): enhance AddWorkSpace component with multi-step functionality

--- src/app/[locale]/get-started/components/AddWorkSpace/index.tsx
+++ src/app/[locale]/get-started/components/AddWorkSpace/index.tsx
@@ -1,39 +1,61 @@
 import {
   Button,
-  Input,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalHeader,
-  Select,
-  SelectItem,
 } from "@nextui-org/react";
 import React from "react";
 
+import RenderCondition from "@/components/common/RenderCondition";
 import RowSteps from "@/components/common/RowSteps";
 
-const animals = [
-  { key: "cat", label: "Cat" },
-  { key: "dog", label: "Dog" },
-  { key: "elephant", label: "Elephant" },
-  { key: "lion", label: "Lion" },
-  { key: "tiger", label: "Tiger" },
-  { key: "giraffe", label: "Giraffe" },
-  { key: "dolphin", label: "Dolphin" },
-  { key: "penguin", label: "Penguin" },
-  { key: "zebra", label: "Zebra" },
-  { key: "shark", label: "Shark" },
-  { key: "whale", label: "Whale" },
-  { key: "otter", label: "Otter" },
-  { key: "crocodile", label: "Crocodile" },
-];
+import Create from "./Create";
 
 interface IProps {
   isOpen: boolean;
   onOpenChange: () => void;
 }
+
+enum Step {
+  CREATE = 0,
+  REVIEW = 1,
+  PUBLIC = 2,
+}
+
+const stepsList = () => [
+  {
+    title: "Create",
+  },
+  {
+    title: "Review",
+  },
+  {
+    title: "Public",
+  },
+];
+
 export default function AddWorkSpace({ isOpen, onOpenChange }: IProps) {
+  const [step, setStep] = React.useState<number>(0);
+  const renderTitleButtonNext = step === Step.CREATE ? "Next" : "Create";
+  const renderTitleButtonPrev = step === Step.CREATE ? "Close" : "Previous";
+
+  const renderActionNext = () => {
+    if (step === Step.CREATE) {
+      setStep(Step.REVIEW);
+    } else if (step === Step.REVIEW) {
+      setStep(Step.PUBLIC);
+    }
+  };
+  const renderActionPrev = (onClose: () => void) => {
+    if (step === Step.CREATE) {
+      onClose();
+    } else if (step === Step.REVIEW) {
+      setStep(Step.CREATE);
+    }
+  };
+
   return (
     <Modal size="4xl" isOpen={isOpen} onOpenChange={onOpenChange}>
       <ModalContent>
@@ -42,43 +64,66 @@ export default function AddWorkSpace({ isOpen, onOpenChange }: IProps) {
             <ModalHeader className="flex flex-col gap-1">
               Add a new workspace
             </ModalHeader>
-            <ModalBody className="flex flex-col items-center text-center">
+            <ModalBody className="flex flex-col items-center gap-0 text-center">
               <p className="text-sm text-gray-500">
                 Fill in your organization details and kickstart efficient
                 collaboration on Lark.
               </p>
-              <RowSteps
-                defaultStep={2}
-                steps={[
-                  {
-                    title: "Create",
-                  },
-                  {
-                    title: "Review",
-                  },
-                  {
-                    title: "Publish",
-                  },
-                ]}
-              />
-              <Input
-                labelPlacement="outside"
-                label="Name"
-                placeholder="Organization name"
-              />
-              <Select labelPlacement="outside" label="Select an animal">
-                {animals.map((animal) => (
-                  <SelectItem key={animal.key}>{animal.label}</SelectItem>
-                ))}
-              </Select>
+              <>
+                <RowSteps
+                  className="my-5"
+                  defaultStep={step}
+                  currentStep={step}
+                  onStepChange={(newStep) => setStep(newStep)}
+                  steps={stepsList()}
+                />
+                <>
+                  <RenderCondition
+                    keepAlive
+                    condition={step === Step.CREATE}
+                    ifContent={<Create />}
+                  />
+                  <RenderCondition
+                    keepAlive
+                    condition={step === Step.REVIEW}
+                    ifContent={<Create />}
+                  />
+                  <RenderCondition
+                    keepAlive
+                    condition={step === Step.PUBLIC}
+                    ifContent={<Create />}
+                  />
+                </>
+              </>
             </ModalBody>
             <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
-                Close
-              </Button>
-              <Button color="primary" onPress={onClose}>
-                Action
-              </Button>
+              <RenderCondition
+                condition={step !== Step.PUBLIC}
+                ifContent={
+                  <>
+                    <Button
+                      color="danger"
+                      onPress={() => renderActionPrev(onClose)}
+                    >
+                      {renderTitleButtonPrev}
+                    </Button>
+                    <Button color="primary" onPress={() => renderActionNext()}>
+                      {renderTitleButtonNext}
+                    </Button>
+                  </>
+                }
+                elseContent={
+                  <Button
+                    color="primary"
+                    onPress={() => {
+                      onClose();
+                      setStep(Step.CREATE);
+                    }}
+                  >
+                    Close
+                  </Button>
+                }
+              />
             </ModalFooter>
           </>
         )}
