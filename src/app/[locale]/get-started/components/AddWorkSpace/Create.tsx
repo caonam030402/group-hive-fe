2e9e05a36d6da94399fc35dc78d68abada71ea85
unknown <nam52871@donga.edu.vn>
2025-01-01 11:05:47 +0700

    feat(get-started): update workspace creation form with dynamic options and simplify modal structure

--- src/app/[locale]/get-started/components/AddWorkSpace/Create.tsx
+++ src/app/[locale]/get-started/components/AddWorkSpace/Create.tsx
@@ -3,25 +3,15 @@ import { Input } from "@nextui-org/input";
 import { Checkbox, Form } from "@nextui-org/react";
 import React from "react";
 
-const animals = [
-  { key: "cat", label: "Cat" },
-  { key: "dog", label: "Dog" },
-  { key: "elephant", label: "Elephant" },
-  { key: "lion", label: "Lion" },
-  { key: "tiger", label: "Tiger" },
-  { key: "giraffe", label: "Giraffe" },
-  { key: "dolphin", label: "Dolphin" },
-  { key: "penguin", label: "Penguin" },
-  { key: "zebra", label: "Zebra" },
-  { key: "shark", label: "Shark" },
-  { key: "whale", label: "Whale" },
-  { key: "otter", label: "Otter" },
-  { key: "crocodile", label: "Crocodile" },
-];
+import {
+  countriesList,
+  industriesList,
+  sizeWorkplaceList,
+} from "@/constants/common";
 
 export default function Create() {
   return (
-    <Form className="flex flex-col gap-5">
+    <Form className="my-3 flex w-full flex-col gap-5">
       <Input
         labelPlacement="outside"
         label="Name"
@@ -32,9 +22,9 @@ export default function Create() {
         placeholder="Select your industry"
         label="Industry"
       >
-        {animals.map((animal) => (
-          <AutocompleteItem value="" key={animal.key}>
-            {animal.label}
+        {industriesList.map((industry) => (
+          <AutocompleteItem value="" key={industry.category}>
+            {industry.category}
           </AutocompleteItem>
         ))}
       </Autocomplete>
@@ -43,9 +33,9 @@ export default function Create() {
         placeholder="Select your size"
         label="Size"
       >
-        {animals.map((animal) => (
-          <AutocompleteItem value="" key={animal.key}>
-            {animal.label}
+        {sizeWorkplaceList.map((size) => (
+          <AutocompleteItem value={size.value} key={size.value}>
+            {size.name}
           </AutocompleteItem>
         ))}
       </Autocomplete>
@@ -54,14 +44,13 @@ export default function Create() {
         placeholder="Select your region"
         label="Region"
       >
-        {animals.map((animal) => (
-          <AutocompleteItem value="" key={animal.key}>
-            {animal.label}
+        {countriesList.map((country) => (
+          <AutocompleteItem value={country.value} key={country.value}>
+            {country.name}
           </AutocompleteItem>
         ))}
       </Autocomplete>
-      <Checkbox size="sm" classNames={{ label: "text-sm" }} defaultSelected>
-        {" "}
+      <Checkbox size="sm" classNames={{ label: "text-xs" }} defaultSelected>
         I have read and accept the Terms of Service and Privacy Policy.
       </Checkbox>
     </Form>
--- src/app/[locale]/get-started/components/AddWorkSpace/index.tsx
+++ src/app/[locale]/get-started/components/AddWorkSpace/index.tsx
@@ -8,9 +8,6 @@ import {
 } from "@nextui-org/react";
 import React from "react";
 
-import RenderCondition from "@/components/common/RenderCondition";
-import RowSteps from "@/components/common/RowSteps";
-
 import Create from "./Create";
 
 interface IProps {
@@ -18,112 +15,23 @@ interface IProps {
   onOpenChange: () => void;
 }
 
-enum Step {
-  CREATE = 0,
-  REVIEW = 1,
-  PUBLIC = 2,
-}
-
-const stepsList = () => [
-  {
-    title: "Create",
-  },
-  {
-    title: "Review",
-  },
-  {
-    title: "Public",
-  },
-];
-
 export default function AddWorkSpace({ isOpen, onOpenChange }: IProps) {
-  const [step, setStep] = React.useState<number>(0);
-  const renderTitleButtonNext = step === Step.CREATE ? "Next" : "Create";
-  const renderTitleButtonPrev = step === Step.CREATE ? "Close" : "Previous";
-
-  const renderActionNext = () => {
-    if (step === Step.CREATE) {
-      setStep(Step.REVIEW);
-    } else if (step === Step.REVIEW) {
-      setStep(Step.PUBLIC);
-    }
-  };
-  const renderActionPrev = (onClose: () => void) => {
-    if (step === Step.CREATE) {
-      onClose();
-    } else if (step === Step.REVIEW) {
-      setStep(Step.CREATE);
-    }
-  };
-
   return (
-    <Modal size="2xl" isOpen={isOpen} onOpenChange={onOpenChange}>
+    <Modal size="xl" isOpen={isOpen} onOpenChange={onOpenChange}>
       <ModalContent>
         {(onClose) => (
           <>
             <ModalHeader className="flex flex-col gap-1">
               Add a new workspace
             </ModalHeader>
-            <ModalBody className="flex flex-col items-center gap-0 text-center">
-              <p className="text-sm text-gray-500">
-                Fill in your organization details and kickstart efficient
-                collaboration on Lark.
-              </p>
-              <>
-                <RowSteps
-                  className="my-5"
-                  defaultStep={step}
-                  currentStep={step}
-                  onStepChange={(newStep) => setStep(newStep)}
-                  steps={stepsList()}
-                />
-                <>
-                  <RenderCondition
-                    keepAlive
-                    condition={step === Step.CREATE}
-                    ifContent={<Create />}
-                  />
-                  <RenderCondition
-                    keepAlive
-                    condition={step === Step.REVIEW}
-                    ifContent={<Create />}
-                  />
-                  <RenderCondition
-                    keepAlive
-                    condition={step === Step.PUBLIC}
-                    ifContent={<Create />}
-                  />
-                </>
-              </>
+            <ModalBody className="flex flex-col gap-5">
+              <Create />
             </ModalBody>
             <ModalFooter>
-              <RenderCondition
-                condition={step !== Step.PUBLIC}
-                ifContent={
-                  <>
-                    <Button
-                      color="danger"
-                      onPress={() => renderActionPrev(onClose)}
-                    >
-                      {renderTitleButtonPrev}
-                    </Button>
-                    <Button color="primary" onPress={() => renderActionNext()}>
-                      {renderTitleButtonNext}
-                    </Button>
-                  </>
-                }
-                elseContent={
-                  <Button
-                    color="primary"
-                    onPress={() => {
-                      onClose();
-                      setStep(Step.CREATE);
-                    }}
-                  >
-                    Close
-                  </Button>
-                }
-              />
+              <Button color="danger" variant="light" onPress={onClose}>
+                Close
+              </Button>
+              <Button color="primary">Create</Button>
             </ModalFooter>
           </>
         )}
